28c79bbeab0510d8442a2d3cac3ad37d75d3e17a
marius jasiunas <mariusmeik@gmail.com>
2022-12-06 11:48:33 +0200

    some commented code to have as a refference

--- tests/UnitTests/Class1.cs
+++ tests/UnitTests/Class1.cs
@@ -2,6 +2,12 @@
 {
     public class Class1
     {
+        //_refreshService.Verify(x => x.InitiateRefreshAsync(), Times.Once);
+
+        //_currencyExchangeRateRepositoryMock.Setup(x => x.GetByNameAndDateAsync("USD", new DateTime(2022, 10, 16)))
+        //                                    .Returns(Task.FromResult(firstExchangeRate));
+
+        // Assert.That(ex.Message, Is.EqualTo("Fetched rates object was null!"));
 
     }
 }
